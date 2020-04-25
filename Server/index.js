e8b98f50a606498d6150cb76dba37e177216e498
Tanishka Dahiya <tanishka.dahiya@nagarro.com>
2020-04-25 17:08:43 +0530

    api for creating new user

---
+++ Server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+var cors = require("cors");
+const app = express();
+const path = require("path");
+//connect Database
+connectDB();
+//Init Middleware
+app.use(cors());
+app.use(express.json({ extended: false }));
+
+//Define Routes
+app.use("/api/users", require("./api/users"));
+
+
+//serve static assets in production
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started ${PORT}`));
