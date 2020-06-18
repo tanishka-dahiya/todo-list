d0cf93bb2602e7ea84a90333fa75a2de850b9d1e
Tanishka Dahiya <tanishka.dahiya@nagarro.com>
2020-06-18 18:10:06 +0530

    final

--- toto-list/src/App.js
+++ toto-list/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Todo from './containers/todoForm'
 import './App.css';
+import Navbar from './components/Navbar';
 
+function App() {
 
-return (
-  <div className="App">
-    <Navbar />
-    <Todo />
+  return (
+    <div className="App">
+      <Navbar />
+      <Todo />
 
-  </div>
-);
-
+    </div>
+  );
+}
 
 export default App;
-;
